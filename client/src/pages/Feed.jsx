7d19a7f0ff13ef22258df5f4d440c51014f8872e
Dereknyren <drdereknyren@gmail.com>
2023-07-12 09:49:36 -0700

    changed new post form to include correct fields and drop down menu for continents

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,15 +1,15 @@
 //import material UI
-import React from 'react';
-import { Container, Grid } from '@mui/material'
+import React from "react";
+import { Container, Grid } from "@mui/material";
 
-//import dependencies 
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+//import dependencies
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 // import utilities
-import useAppStore from '../store/appStore';
-import Post from '../components/post';
-import Header from '../components/header';
+import useAppStore from "../store/appStore";
+import Post from "../components/post";
+import Header from "../components/header";
 
 //import post creation design
 import Button from "@mui/material/Button";
@@ -19,6 +19,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import Autocomplete from "@mui/material/Autocomplete";
 
 /*
 toggleDashboardAuth default = true, use for production
@@ -27,79 +28,106 @@ toggleDashboardAuth = false, deactivates dashboard page authorization
 */
 const toggleFeedAuth = true;
 
- const Feed = () => {
-
+const Feed = () => {
   const navigate = useNavigate();
-// hooks
-const [imgURL, setImgURL] = useState('');
-const [location, setLocation] = useState('');
-const [review, setReview] = useState('');
-const [rating, setRating] = useState(0);
+  // hooks
+  const [imgURL, setImgURL] = useState("");
+  const [location, setLocation] = useState("");
+  const [review, setReview] = useState("");
+  const [rating, setRating] = useState(0);
+  const [title, setTitle] = useState("")
   //check if user has logged in
   const isLoggedIn = useAppStore((state) => state.isLoggedIn);
   const userData = useAppStore((state) => state.userData);
 
-    // check user authorization
-    useEffect(() => {
-      if (!isLoggedIn && toggleFeedAuth) navigate('/');
-    }, []);
+  // check user authorization
+  useEffect(() => {
+    if (!isLoggedIn && toggleFeedAuth) navigate("/");
+  }, []);
 
+  //
+  const [open, setOpen] = React.useState(false);
 
-    //
-    const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClickOpen = () => {
-      setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handlePost = () => {
+    const postData = {
+      author_id: 24,
+      content: 1,
+      image: imgURL,
+      title: "we dont have titles rip",
+      rating,
+      content: review,
     };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    const handlePost = () => {
-      const postData = {
-        author_id: 24,
-        content: 1,
-        image: imgURL,
-        title: "we dont have titles rip",
-        rating,
-        content: review
-      }
-      fetch('/api/feed', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(postData),
-      })
+    fetch("/api/feed", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
       .then((data) => data.json())
       .then((res) => {
-        // TODO: 
-          // create post passing in data from fetch
-          // unshift posts array passing post to put users new post on top of feed
-        console.log("this is RES", res)
+        // TODO:
+        // create post passing in data from fetch
+        // unshift posts array passing post to put users new post on top of feed
+        console.log("this is RES", res);
       })
       .catch((error) => {
         // Handle any errors
         console.log(error);
       });
-      
-    };
+  };
 
-    
-    return (
-      <Grid container justifyContent={"center"}>
-        <div>
-          <Header />
-          <Grid container justifyContent={"center"}>
+  const continents = [
+    "North America",
+    "South America",
+    "Europe",
+    "Asia",
+    "Africa",
+    "Australia",
+    "Antarctica",
+  ];
+
+  return (
+    <Grid container justifyContent={"center"}>
+      <div>
+        <Header />
+        <Grid container justifyContent={"center"}>
           <div>
-            <Button sx={{m: 2}} variant="contained" onClick={handleClickOpen}>
+            <Button sx={{ m: 2 }} variant="contained" onClick={handleClickOpen}>
               New Post
             </Button>
             <Dialog open={open} onClose={handleClose}>
-              <DialogTitle>Create Post</DialogTitle>
+              <DialogTitle>Tell us about your trip!</DialogTitle>
               <DialogContent>
-                <DialogContentText>Tell us about your trip!</DialogContentText>
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  options={continents}
+                  sx={{ width: 300 }}
+                  onChange={(e, value) => setLocation(value)}
+                  renderInput={(params) =>
+                    <TextField {...params} label="Location" />
+                  }
+                />
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  label="Title"
+                  type="email"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  fullWidth
+                  variant="standard"
+                />
                 <TextField
                   autoFocus
                   margin="dense"
@@ -115,17 +143,6 @@ const [rating, setRating] = useState(0);
                   autoFocus
                   margin="dense"
                   id="name"
-                  label="Location"
-                  type="email"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                  fullWidth
-                  variant="standard"
-                />
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="name"
                   label="Review"
                   type="email"
                   value={review}
@@ -134,10 +151,11 @@ const [rating, setRating] = useState(0);
                   variant="standard"
                 />
                 <TextField
+                  sx={{mt: 2}}
                   autoFocus
                   margin="dense"
                   id="name"
-                  label="Rating 1-5"
+                  label="Rating 0-5"
                   type="email"
                   value={rating}
                   onChange={(e) => setRating(Number(e.target.value))}
@@ -151,12 +169,11 @@ const [rating, setRating] = useState(0);
               </DialogActions>
             </Dialog>
           </div>
-          </Grid>
-          <Post />
-        </div>
-      </Grid>
-      
-    )
-}
+        </Grid>
+        <Post />
+      </div>
+    </Grid>
+  );
+};
 
 export default Feed;
